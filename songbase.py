1d36516d4862d9bc6bfcc09cdfe83e78d4db95b3
mruoff <mruoff@udel.edu>
2017-11-09 13:45:28 -0500

    first commit

--- songbase.py
+++ songbase.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return '<h1>Misy Page on Python</h1>'
+    #return '<h1>Misy Page on Python</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<string:name>/')
 def get_user(name):
-        return '<h1>hello %s your age is %d</h1>' % (name,3)
+        #return '<h1>hello %s your age is %d</h1>' % (name,3)
+        return render_template('user.html', user_name=name)
+
+@app.route('/songs')
+def get_all_songs():
+        songs = [
+        'Back Where I Come From',
+        'Happier',
+        'All too Well'
+        ]
+        return render_template('songs.html', songs=songs)
 
 
 if __name__ == '__main__':
